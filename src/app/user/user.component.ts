2a0779419fc282c5dc0d54464561b780f82b109e
Likith <K14141@ktkbankltd.com>
2024-06-24 18:27:50 +0530

    second update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -24,4 +24,18 @@ export class UserComponent implements OnInit {
   // the use of the randomdata is ** if the page is refreshed then the data of name/id/image displayed changes everytime refreshed randomly**
 
   selectedUser = DUMMY_USERS[randomdata];
+
+  //a method function inside a class to access properties . Note as it is inside a fucntion in class THIS keyword is mandatory in JS
+  // return** is mandatory when get is used in any method
+  get imagePath (){
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  // event listener method for click. when ever clicked , msg is displayed in console
+  onselectuser(){
+    console.log("Clicked !!");
+    // const declared locally only inside this method. when clicked then images and names change without refreshing page everytime
+    const randomdata = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomdata];
+  }
 }
