5bface0e8bd73f6e0d7b1f3f7f1a515a133736bb
Likith <K14141@ktkbankltd.com>
2024-06-29 13:10:34 +0530

    latest changes 29062024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 // import { HeaderComponent } from './header/header.component';
 
+// to define componenet inputs
+import { DUMMY_USERS } from './dummy-users';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +11,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udemy';
+
+  // to define component inputs
+  users = DUMMY_USERS;
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 // import dummy users data to make use in user component
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -11,31 +11,52 @@ const randomdata = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+// class to display avatar and names randomly when clicked
+// export class UserComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+//   // drawing only 1 user from users.ts
+//   // selectedUser = DUMMY_USERS[2];
+
+//   // drawing dynamic users from users.ts upto max length of datas in dummy.users.ts
+//   // the use of the randomdata is ** if the page is refreshed then the data of name/id/image displayed changes everytime refreshed randomly**
+
+//   selectedUser = DUMMY_USERS[randomdata];
+
+//   //a method function inside a class to access properties . Note as it is inside a fucntion in class THIS keyword is mandatory in JS
+//   // return** is mandatory when get is used in any method
+//   get imagePath (){
+//     return 'assets/users/' + this.selectedUser.avatar
+//   }
+
+//   // event listener method for click. when ever clicked , msg is displayed in console
+//   onselectuser(){
+//     console.log("Clicked !!");
+//     // const declared locally only inside this method. when clicked then images and names change without refreshing page everytime
+//     const randomdata = Math.floor(Math.random() * DUMMY_USERS.length);
+//     this.selectedUser = DUMMY_USERS[randomdata];
+//   }
+// }
+
+// class using input decorator binding values and display in user component
 export class UserComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-  // drawing only 1 user from users.ts
-  // selectedUser = DUMMY_USERS[2];
+    ngOnInit() {
+    }
+    // to get the avatar/name value from outside the component i.e., from app component
+@Input () avatar : string;
+@Input () name : string;
 
-  // drawing dynamic users from users.ts upto max length of datas in dummy.users.ts
-  // the use of the randomdata is ** if the page is refreshed then the data of name/id/image displayed changes everytime refreshed randomly**
+ get imagePath (){
+  return 'assets/users/' + this.avatar;
+}
 
-  selectedUser = DUMMY_USERS[randomdata];
+onselectuser(){
 
-  //a method function inside a class to access properties . Note as it is inside a fucntion in class THIS keyword is mandatory in JS
-  // return** is mandatory when get is used in any method
-  get imagePath (){
-    return 'assets/users/' + this.selectedUser.avatar
-  }
-
-  // event listener method for click. when ever clicked , msg is displayed in console
-  onselectuser(){
-    console.log("Clicked !!");
-    // const declared locally only inside this method. when clicked then images and names change without refreshing page everytime
-    const randomdata = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomdata];
-  }
+}
 }
