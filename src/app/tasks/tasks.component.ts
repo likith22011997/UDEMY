8065891151c9f22666b28eae53a3b5a37684555f
Likith <K14141@ktkbankltd.com>
2024-08-27 17:46:26 +0530

    task details

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit ,Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { title } from 'process';
 
 @Component({
   selector: 'app-tasks',
@@ -12,6 +13,29 @@ export class TasksComponent implements OnInit {
   ngOnInit() {
   }
   // accepting input from app component
-@Input() name: string;
-@Input() id: string;
+  @Input() name: string;
+  @Input() id: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all basic and advanced features of angular',
+      duedate: '2025-12-10'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'MSME TL',
+      summary: 'Learn the journey of both CV and NCV',
+      duedate: '2025-09-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Micro Mitra WC',
+      summary: 'Learn the journey of MMWC DPN',
+      duedate: '2025-07-23'
+    }
+  ]
 }
